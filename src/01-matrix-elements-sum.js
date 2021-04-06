c33d1891313541b4a852867518147597cf3ae877
Alexandr Klychnikov <klychnikov75@mail.ru>
2021-04-06 21:16:41 +0300

    fix: correct eslint

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -16,17 +16,16 @@
  */
 function getMatrixElementsSum(matrix) {
   let sum = 0;
-    for (let j = 0; j < matrix[0].length; j++) {
-      for (let i = 0; i < matrix.length; i++) {
-        if (matrix[i][j]) {
-         sum += matrix[i][j];
-        } else {
-          break
-        }   
+  for (let j = 0; j < matrix[0].length; j++) {
+    for (let i = 0; i < matrix.length; i++) {
+      if (matrix[i][j]) {
+        sum += matrix[i][j];
+      } else {
+        break;
       }
-    } 
-  return sum
+    }
+  }
+  return sum;
 }
 
 module.exports = getMatrixElementsSum;
-
